a24efb46c47904eb89afc5ad3b48c7811f11cdb8
Zoran <zoran1991@gmail.com>
2015-10-10 00:01:38 -0700

    default argument values for sleep/water logs

--- lib/fitbyte/sleep.rb
+++ lib/fitbyte/sleep.rb
@@ -1,6 +1,6 @@
 module Fitbyte
   class Client
-    def sleep_logs(date)
+    def sleep_logs(date=Date.today)
       get("1/user/-/sleep/date/#{format_date(date)}.json")
     end
   end
--- lib/fitbyte/water.rb
+++ lib/fitbyte/water.rb
@@ -1,6 +1,6 @@
 module Fitbyte
   class Client
-    def water_logs(date)
+    def water_logs(date=Date.today)
       get("1/user/-/foods/log/water/date/#{format_date(date)}.json")
     end
   end
