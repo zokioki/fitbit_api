0af1dedd415f003b74cd7fc30c55c80617ad3aba
Zoran <zoran1991@gmail.com>
2015-10-10 23:18:46 -0700

    typo fix

--- lib/fitbyte/body.rb
+++ lib/fitbyte/body.rb
@@ -1,7 +1,7 @@
 module Fitbyte
   class Client
     def weight_logs(date=Date.today)
-      get("1/user/-/body/log/weight/date/format_date(date).json")
+      get("1/user/-/body/log/weight/date/#{format_date(date)}.json")
     end
 
     def body_fat_logs(date=Date.today)
