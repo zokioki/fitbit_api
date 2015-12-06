e3f571e84cbbef3194334da3f73e4740410da6f8
Zoran <zoran1991@gmail.com>
2015-12-06 02:36:00 -0800

    add ruby 2.0.0 to list of versions to test against in Travis; change sample rspec test to pass

--- spec/fitbyte_spec.rb
+++ spec/fitbyte_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Fitbyte do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Fitbyte::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it "does something totally useful" do
+    expect(true).to eq(true)
   end
 end
