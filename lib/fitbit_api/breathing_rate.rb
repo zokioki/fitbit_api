3d0ea2757415ad75dac5f2c79dec78f33b9caf65
Zoran Pesic <zoran1991@gmail.com>
2022-11-02 19:58:32 -0700

    add support for breathing rate intraday endpoints

--- lib/fitbit_api/breathing_rate.rb
+++ lib/fitbit_api/breathing_rate.rb
@@ -22,5 +22,28 @@ module FitbitAPI
       # remove root key from response
       result.values[0]
     end
+
+    def breathing_rate_intraday(opts={})
+      date       = opts[:date] || Date.today
+      start_date = opts[:start_date]
+      end_date   = opts[:end_date]
+
+      if start_date && !end_date
+        end_date = Date.today
+      end
+
+      unless date || start_date
+        raise FitbitAPI::InvalidArgumentError, 'A date or start_date and end_date are required.'
+      end
+
+      if start_date
+        result = get("user/#{user_id}/br/date/#{format_date(start_date)}/#{format_date(end_date)}/all.json")
+      else
+        result = get("user/#{user_id}/br/date/#{format_date(date)}/all.json")
+      end
+
+      # remove root key from response
+      result.values[0]
+    end
   end
 end
