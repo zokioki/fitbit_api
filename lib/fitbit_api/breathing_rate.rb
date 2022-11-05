0e62281ae1890754696e27b7ce99c9faeff0139a
Zoran Pesic <zoran1991@gmail.com>
2022-11-05 15:04:55 -0700

    add docs

--- lib/fitbit_api/breathing_rate.rb
+++ lib/fitbit_api/breathing_rate.rb
@@ -1,5 +1,17 @@
 module FitbitAPI
   class Client
+    # Returns the average breathing rate data for a given date or date range.
+    # If both a date and a date range are given, the date range takes precedence.
+    #
+    #   breathing_rate_summary(date: Date.parse('2021-04-16'))
+    #   breathing_rate_summary(start_date: Date.parse('2021-05-18'), end_date: Date.parse('2021-05-24'))
+    #
+    # @param params [Hash] The request parameters
+    #
+    # @option params :date [Date] The target date
+    # @option params :start_date [Date] The start of the date range
+    # @option params :end_date [Date] The end of the date range
+
     def breathing_rate_summary(opts={})
       date       = opts[:date] || Date.today
       start_date = opts[:start_date]
@@ -22,6 +34,18 @@ module FitbitAPI
       # remove root key from response
       result.values[0]
     end
+
+    # Returns the intraday breathing rate data for a given date or date range.
+    # If both a date and a date range are given, the date range takes precedence.
+    #
+    #   breathing_rate_intraday(date: Date.parse('2021-04-16'))
+    #   breathing_rate_intraday(start_date: Date.parse('2021-05-18'), end_date: Date.parse('2021-05-24'))
+    #
+    # @param params [Hash] The request parameters
+    #
+    # @option params :date [Date] The target date
+    # @option params :start_date [Date] The start of the date range
+    # @option params :end_date [Date] The end of the date range
 
     def breathing_rate_intraday(opts={})
       date       = opts[:date] || Date.today
