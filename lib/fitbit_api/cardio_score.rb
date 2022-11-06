afc6980ad52c5aa14ad6bdf35ea8f258a0043b7d
Zoran Pesic <zoran1991@gmail.com>
2022-11-05 22:16:56 -0700

    add docs

--- lib/fitbit_api/cardio_score.rb
+++ lib/fitbit_api/cardio_score.rb
@@ -1,5 +1,17 @@
 module FitbitAPI
   class Client
+    # Returns the cardio fitness score data for a given date or date range.
+    # If both a date and a date range are given, the date range takes precedence.
+    #
+    #   cardio_score_summary(date: Date.parse('2021-04-16'))
+    #   cardio_score_summary(start_date: Date.parse('2021-05-18'), end_date: Date.parse('2021-05-24'))
+    #
+    # @param params [Hash] The request parameters
+    #
+    # @option params :date [Date] The target date
+    # @option params :start_date [Date] The start of the date range
+    # @option params :end_date [Date] The end of the date range
+
     def cardio_score_summary(opts={})
       date       = opts[:date] || Date.today
       start_date = opts[:start_date]
