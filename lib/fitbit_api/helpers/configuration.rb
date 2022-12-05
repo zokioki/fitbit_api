52775a09b611444e86b448b9f0a05ea7b823f2f4
Zoran Pesic <zoran1991@gmail.com>
2022-12-04 22:05:51 -0800

    use define_singleton_method for setting config class methods
    use class instance variables for config values

--- lib/fitbit_api/helpers/configuration.rb
+++ lib/fitbit_api/helpers/configuration.rb
@@ -7,22 +7,14 @@ module FitbitAPI
     end
 
     def define_setting(name, default = nil)
-      class_variable_set("@@#{name}", default)
+      instance_variable_set("@#{name}", default)
 
-      define_class_method "#{name}=" do |value|
-        class_variable_set("@@#{name}", value)
+      define_singleton_method("#{name}=") do |value|
+        instance_variable_set("@#{name}", value)
       end
 
-      define_class_method name do
-        class_variable_get("@@#{name}")
-      end
-    end
-
-    private
-
-    def define_class_method(name, &block)
-      (class << self; self; end).instance_eval do
-        define_method name, &block
+      define_singleton_method(name) do
+        instance_variable_get("@#{name}")
       end
     end
   end
